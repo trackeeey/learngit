db8207c37719c984b71c3bf248e0d07096f0fbd7
Your Name <email@example.com>
2017-03-08 16:10:27 +0800

    123.456----int

--- shiyan1.py
+++ shiyan1.py
@@ -22,3 +22,18 @@ def a(max):
 	return 'done'
 
 print (a(100))
+
+
+#把‘123.456’变为123.456整形数据
+
+from functools import reduce
+
+def a(s):
+	def b(m,n):
+		return m*10+n
+	x=s.index('.')     #利用 index() 函数确定字符串 S 中 ‘.’的位置
+	S1=list(map(int,[i for i in s[:x]]))   #分割整数部分
+	S2=list(map(int,[i for i in s[x+1:]]))   #分割小数部分
+	return reduce(b,S1)+reduce(b,S2)/10**len(S2)  #根据位数len()来添加小数点
+
+print (a('123.456'))
